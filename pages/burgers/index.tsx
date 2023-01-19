6f2fecb81f9b08066f9dadddcc149058ddfa6b03
Alexey Pavlov <pavlov.aleksey.m@gmail.com>
2023-01-19 22:35:21 +0100

    feat: add db with burgers & script to run app and db api

--- pages/burgers/index.tsx
+++ pages/burgers/index.tsx
@@ -1,6 +1,28 @@
-const Burgers = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('http://localhost:5000/items');
+    const data = await res.json();
+
+    return {
+        props: {
+            burgers: data
+        }
+    }
+}
+
+type Burger = {
+    name: string;
+    image: string;
+    desc: string;
+    price: number;
+    id: string;
+}
+
+const Burgers = (props: Burger[]) => {
+    console.log('props >>> ', props)
+
     return (<>
         <h1>Our Burgers</h1>
+
     </>)
 }
 
