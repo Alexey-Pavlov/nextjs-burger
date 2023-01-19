b6450bdf8fa6b74477de6b1c7e3216969815db98
Alexey Pavlov <pavlov.aleksey.m@gmail.com>
2023-01-19 20:17:09 +0100

    feat: add header & footer on all pages

---
+++ components/Layout.tsx
@@ -0,0 +1,18 @@
+import {ReactNode} from "react";
+import Header from './Header'
+import Footer from './Footer'
+
+
+type LayoutProps = {
+    children?: ReactNode
+}
+
+const Layout = ({children}: LayoutProps) => {
+    return (<div className="content">
+        <Header/>
+        {children}
+        <Footer/>
+    </div>)
+}
+
+export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 import Link from 'next/link'
 
 export default function Home() {
     return (<>
         <div>
-            <Header/>
             <h1>Main</h1>
             <Link href='/burgers'>All burgers</Link>
-            <Footer/>
         </div>
     </>)
 }
