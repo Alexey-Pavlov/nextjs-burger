5c88b53a712dde11cf59b97d72e137472cdea92a
Alexey Pavlov <pavlov.aleksey.m@gmail.com>
2023-01-19 20:35:29 +0100

    feat: add styles & home text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (<>
-        <div>
-            <h1>Main</h1>
-            <Link href='/burgers'>All burgers</Link>
+        <div className={styles.container}>
+            <h1 className={`${styles.title} font-effect-fire-animation`}>Main</h1>
+            <p className={styles.text}>What is the perfect burger? Fresh lettuce, soft buns, juicy meat. You can argue
+                about other components of the filling, because this is a matter of taste.</p>
+            <p className={styles.text}>There are a couple of other factors that affect appetite: prices, quality of
+                service, the right atmosphere of the establishment.</p>
+            <Link href='/burgers' className={styles.btn}>All burgers</Link>
         </div>
     </>)
 }
